cf889e315a74428e77b51d436025c59b036534bc
Oleksandr Bondarenko <olb@interlogic.com.ua>
2022-10-23 02:37:47 +0300

    improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,6 +19,6 @@ tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout -f "$tag"
 git pull origin "$tag"
 
-sudo docker-compose up --detach
+sudo docker-compose up -d
 
 exit 0
