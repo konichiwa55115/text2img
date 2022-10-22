904a34414b4d176e9ee9b7ece78c2e00a36aa6d8
Oleksandr Bondarenko <olb@interlogic.com.ua>
2022-10-23 01:44:53 +0300

    refactroring docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,13 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
 cd "$scriptDir/.." || exit 1
 
+tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
 sudo docker build -t iamsashko/text2img .
 
 sudo docker login
 
-sudo docker push iamsashko/text2img:latest
+sudo docker push iamsashko/text2img:"$tag"
 
 cd "$currentDir" || exit 1
 
